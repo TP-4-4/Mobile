65305d9ce650202921e1453f07e022cb05cb3f1d
k8lobova <llkk20030808@gmail.com>
2024-05-11 12:30:36 +0300

    db

--- bd/database.py
+++ bd/database.py
@@ -1,13 +1,13 @@
 import ssl
 
+import certifi
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from bd import host
-ssl_context = ssl.create_default_context()
+ssl_context = ssl._create_unverified_context()
 DATABASE_URL = f'postgresql+pg8000://{host.username}:{host.password}@{host.hostname}/{host.database}'
 engine = create_engine(DATABASE_URL, connect_args={"ssl_context": ssl_context})
-
 
 # Создаем фабрику сессий SQLAlchemy
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
