46094fd703041f01228fbf174c7aa52692dba3ca
k8lobova <llkk20030808@gmail.com>
2024-05-11 13:13:57 +0300

    db

--- bd/database.py
+++ bd/database.py
@@ -5,7 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from bd import host
+
 ssl_context = ssl._create_unverified_context()
+#ssl_context = ssl.create_default_context()
 DATABASE_URL = f'postgresql+pg8000://{host.username}:{host.password}@{host.hostname}/{host.database}'
 engine = create_engine(DATABASE_URL, connect_args={"ssl_context": ssl_context})
 
@@ -14,14 +16,3 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Создаем базовый класс для всех моделей
 Base = declarative_base()
-
-# # Укажите свои параметры подключения
-# conn = pg8000.connect(
-#     user=host.username,
-#     password=host.password,
-#     host=host.hostname,
-#     database=host.database,
-#     port=5432,
-#     ssl=ssl.create_default_context()
-# )
-#
